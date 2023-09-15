8934e86327a8272b1861f0cc0909875ab2a4913b
saurav kumar <74324530+Crispae@users.noreply.github.com>
2023-09-15 14:34:17 +0200

    Update abner_app.py

--- abner_app.py
+++ abner_app.py
@@ -11,38 +11,43 @@ t = Tagger()
 def extract_entities_from_text(text):
     # Get the entities using abner
     annotated_text = t.getEntities(text.strip())
-    entities = annotated_text[0]
+    entities = list(set(annotated_text[0]))
 
-    # Create a regex pattern to find the entities in the text
-    pattern = r"|".join(entities)
-    matches = re.finditer(pattern, text)
+    ## condition to check, entites list is not null
+    if entities:
+        # Create a regex pattern to find the entities in the text
+        pattern = r"|".join(entities)
+        matches = re.finditer(pattern, text)
 
-    entities_list = []
-    for idx, match in enumerate(matches):
-        start_index = match.start()
-        end_index = match.end()
-        match_text = match.group()
-        entity_type = annotated_text[1][idx]
+        entities_list = []
+        for idx, match in enumerate(matches):
+            start_index = match.start()
+            end_index = match.end()
+            match_text = match.group()
+            entity_type = annotated_text[1][idx]
 
-        entities_dict = {
-            "start": start_index,
-            "end": end_index,
-            "word": match_text,
-            "entity_type": entity_type.lower()
-        }
-        entities_list.append(entities_dict)
+            entities_dict = {
+                "start": start_index,
+                "end": end_index,
+                "word": match_text,
+                "entity_type": entity_type.lower()
+            }
 
-    return entities_list
+            ## appending entites to the list
+            entities_list.append(entities_dict)
+
+        return entities_list
+
+    else:
+        return None
 
 @app.route('/extract_entities', methods=['POST'])
 def extract_entities():
     text = request.json.get('text')
-    
     try:
         entities = extract_entities_from_text(text)
-        print(entities)
         return jsonify({"Result":entities})
-    except IndexError:
+    except:
         print("Error occured while processing the text, the tokens may cross the size limitation")
         return jsonify({"Result":"Error"})
 
